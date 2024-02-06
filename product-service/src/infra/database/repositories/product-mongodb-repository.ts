dcfdaa104ef7f0ca06ed6f286099651452635f0a
Matheus Santos <maatheus.santos223@gmail.com>
2024-02-06 15:37:22 -0300

    feat: add ProductMongoDBRepository

---
+++ product-service/src/infra/database/repositories/product-mongodb-repository.ts
@@ -0,0 +1,38 @@
+import { BuyProductRepository } from '../../../data/protocols/db-buy-product';
+import { CreateProductRepository } from '../../../data/protocols/db-create-product';
+import { Product } from '../../../domain/models/product';
+import { CreateProductModel } from '../../../domain/use-cases/create-product';
+import { BuyProductModel } from '../../../domain/use-cases/buy-product';
+import { ProductModel } from '../../models/product-schema';
+
+export class ProductMongoDBRepository 
+implements 
+    CreateProductRepository,
+    BuyProductRepository
+{
+
+  constructor(
+    private mongoProductModel: typeof ProductModel
+  ) {}
+
+  async create(product: CreateProductModel): Promise<void> {
+    await this.mongoProductModel.create(product);
+  }
+
+  async buy(products: BuyProductModel): Promise<void> {
+    try {
+      console.log(products);
+    } catch (error: any) {
+      console.log(error);
+      return error;
+    }
+  }
+
+  async getProductByCode(code: string): Promise<Product | null> {
+    
+    const checkIfCodeAlreadyExists = await this.mongoProductModel.findOne({ code });
+    if(checkIfCodeAlreadyExists) return checkIfCodeAlreadyExists;
+
+    return null; 
+  }
+}
