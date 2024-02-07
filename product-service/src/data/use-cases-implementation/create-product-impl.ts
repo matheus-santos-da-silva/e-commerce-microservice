86289a9d0513a7b1f769800ee03f59764a33c874
Matheus Santos <maatheus.santos223@gmail.com>
2024-02-07 17:30:53 -0300

    feat: add messagingService to CreateProduct useCase

--- product-service/src/data/use-cases-implementation/create-product-impl.ts
+++ product-service/src/data/use-cases-implementation/create-product-impl.ts
@@ -1,11 +1,13 @@
 import { Product } from '../../domain/models/product';
 import { CreateProduct, CreateProductModel, CreateProductResponse } from '../../domain/use-cases/create-product';
+import { MessagingServiceRepository } from '../../infra/rabbitMQ/protocols/messaging-service-repository';
 import { CreateProductRepository } from '../protocols/db-create-product';
 
 export class CreateProductImplementation implements CreateProduct {
 
   constructor(
-    private createProductRepository: CreateProductRepository
+    private createProductRepository: CreateProductRepository,
+    private messagingService: MessagingServiceRepository
   ) {}
 
   async create({
@@ -15,18 +17,23 @@ export class CreateProductImplementation implements CreateProduct {
     quantity
   }: CreateProductModel): Promise<CreateProductResponse> {
 
-    const product = await this.createProductRepository.getProductByCode(code);
-    if(product) return {
+    const checkProductCode = await this.createProductRepository.getProductByCode(code);
+    if(checkProductCode) return {
       message: 'Code is already exists',
       statusCode: 422
     };
 
-    await this.createProductRepository.create( new Product({
+    const product = new Product({
       code,
       name,
       price,
       quantity
-    })); 
+    }); 
+
+    await this.createProductRepository.create( product ); 
+  
+    await this.messagingService.start();
+    await this.messagingService.publishInQueue( 'PRODUCT', JSON.stringify(product) );
 
     return { message: 'Product created successfully', statusCode: 201 };
 
