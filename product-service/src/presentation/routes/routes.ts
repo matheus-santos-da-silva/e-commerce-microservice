8aec58cdab88fefd5631df32038387da95d99dbd
Matheus Santos <maatheus.santos223@gmail.com>
2024-02-07 17:31:45 -0300

    feat: add messagingRepository to create route

--- product-service/src/presentation/routes/routes.ts
+++ product-service/src/presentation/routes/routes.ts
@@ -3,15 +3,17 @@ import { CreateProductController } from '../controllers/create-product-controlle
 import { Request, Response } from 'express'; 
 import { CreateProductImplementation } from '../../data/use-cases-implementation/create-product-impl';
 import { ProductMongoDBRepository } from '../../infra/database/repositories/product-mongodb-repository';
-import { ProductModel } from '../../infra/models/product-schema'; 
+import { ProductModel } from '../../infra/database/models/product-schema'; 
 import { validationMiddleware } from '../../data/utils/middleware/validation';
 import { CreateProductValidationSchema } from '../../data/utils/validations-schemas/create-product-validation-schema';
+import { RabbitMQMessagingService } from '../../infra/rabbitMQ/rabbitMQ';
 
 const router = Router();
 
 router.post('/create', validationMiddleware(CreateProductValidationSchema) , async (request: Request, response: Response) => {
   const mongoRepository = new ProductMongoDBRepository(ProductModel);
-  const createProductUseCase = new CreateProductImplementation(mongoRepository);
+  const messagingRepository = new RabbitMQMessagingService(`${process.env.URI}`);
+  const createProductUseCase = new CreateProductImplementation(mongoRepository, messagingRepository);
   const controller = new CreateProductController(createProductUseCase);
 
   await controller.execute(request, response);
