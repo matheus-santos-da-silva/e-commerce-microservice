ab914baa9858baac9d7de57fe7ea765f2c99cea8
Matheus Santos <maatheus.santos223@gmail.com>
2024-02-19 16:49:01 -0300

    feat: add customer-mongodb-repository

---
+++ order-service/src/infra/database/repositories/customer/customer-mongodb-repository.ts
@@ -0,0 +1,15 @@
+import { CustomerRepository } from '../../../../data/protocols/customer/db-customer-repository';
+import { Customer } from '../../../../domain/models/customer';
+import { CustomerModel } from '../../models/customer.schema';
+
+export class CustomerMongoDBRepository implements CustomerRepository {
+
+  constructor(
+    private customerModel: typeof CustomerModel
+  ) {}
+
+  async create({ email, externalId, name }: Customer): Promise<void> {
+    await this.customerModel.create({ email, externalId, name });
+  }
+}
+  
